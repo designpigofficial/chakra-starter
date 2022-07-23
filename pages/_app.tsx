89c67911b8bc6f1226bbd8bce7d24bd565b64534
designpigofficial <89279319+designpigofficial@users.noreply.github.com>
2022-07-22 20:22:12 -0600

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+import Navbar from '../src/components/Navbar'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const colors = {
+  brand: {
+    primaryColor: 'red',
+    secondaryColor: 'blue',
+    tertiaryColor: 'yellow',
+    light: '#eaeaea',
+    dark: '#303030',
+    white: '#ffffff',
+    black: '#000000',
+  },
+}
+
+const theme = extendTheme ({ colors })
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <ChakraProvider>
+      <Navbar  />
+    <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
